8653b17a1e8c8743390a6fdd201532f5e97da5d7
Abdulrahman Alkhateeb <abdoh.alkhateeb@gmail.com>
2023-10-19 21:31:36 +0300

    Remove unused struct

--- monty.h
+++ monty.h
@@ -43,22 +43,7 @@ typedef struct stack_s
 	struct stack_s *next;
 } stack_t;
 
-/**
- * struct instruction_s - opcode and its function
- * @opcode: the opcode
- * @f: function to handle the opcode
- *
- * Description: opcode and its function
- * for stack, queues, LIFO, FIFO
- */
-typedef struct instruction_s
-{
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
-
 extern stack_t *stack;
-extern instruction_t instructions[];
 
 void interpret_file(char *filename);
 char **parse_line(char *line);
